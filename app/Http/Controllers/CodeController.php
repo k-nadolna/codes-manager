fc71efdc317d06554679a55f954c287104882f6e
Katarzyna Nadolna <katarzyna.nadolna111@gmail.com>
2026-02-18 18:12:14 +0000

    Code controller update

--- app/Http/Controllers/CodeController.php
+++ app/Http/Controllers/CodeController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\Code;
 use App\Services\CodeService;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
 
 class CodeController extends Controller
 {
@@ -16,7 +18,8 @@ class CodeController extends Controller
         $this->codeService = $codeService;
     }
 
-    function index(){
+    public function index(): View
+    {
         $codes = Code::where('user_id', Auth::id())
                     ->with('user')
                     ->paginate(10);
@@ -24,25 +27,30 @@ class CodeController extends Controller
         return view('index', compact('codes'));
     }
 
-    function create(){
+    public function create(): View
+    {
         return view('create');
     }
 
-    function store(Request $request){
+    public function store(Request $request): RedirectResponse
+    {
          $validated = $request->validate([
             'quantity' => 'required|integer|min:1|max:10',
         ]);
 
         $generated = $this->codeService->generateMultipleCodes($validated['quantity']);
 
-        return back()->with('success', 'Kody zostały pomyślnie wygenerowane');
+        return redirect()
+                    ->route('index')
+                    ->with('success', 'Kody zostały pomyślnie wygenerowane');
     }
 
-    function delete(){
+    public function delete(){
         return view('delete');
     }
 
-    function destroy(Request $request){
+    public function destroy(Request $request): RedirectResponse
+    {
         $request->validate([
             'codes_to_delete' => 'required|string',
         ]);
