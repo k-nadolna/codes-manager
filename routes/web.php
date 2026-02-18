7ce6ae4f3931830595153e858f378058e317aed1
Katarzyna Nadolna <katarzyna.nadolna111@gmail.com>
2026-02-18 18:48:41 +0000

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -5,19 +5,25 @@ use App\Http\Controllers\CodeController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/auth/register', [AuthController::class, 'showRegister'])->name('auth.register.form');
-Route::post('/auth/register', [AuthController::class, 'register'])->name('auth.register');
-Route::get('/auth/login', [AuthController::class, 'showLogin'])->name('auth.login.form');
-Route::post('/auth/login', [AuthController::class, 'login'])->name('auth.login');
-Route::post('/auth/logout', [AuthController::class, 'logout'])->name('auth.logout');
+Route::prefix('auth')
+    ->controller(AuthController::class)
+    ->group(function(){
+      Route::get('register', 'showRegister')->name('auth.register.form');
+      Route::post('register', 'register')->name('auth.register');
+      Route::get('login', 'showLogin')->name('auth.login.form');
+      Route::post('login', 'login')->name('auth.login');
+      Route::post('logout', 'logout')->name('auth.logout');
+});
 
 Route::redirect('/', '/auth/login');
 
-Route::middleware(['auth'])->group(function() {
-  Route::get('/codes', [CodeController::class, 'index'])->name('index');
-  Route::get('/codes/create', [CodeController::class, 'create']);
-  Route::post('/codes/store', [CodeController::class, 'store'])->name('codes.store');
-  Route::get('/codes/delete', [CodeController::class, 'delete'])->name('codes.delete');
-  Route::post('/codes/destroy', [CodeController::class, 'destroy'])->name('codes.destroy');
+Route::middleware(['auth'])
+    ->prefix('codes')
+    ->controller(CodeController::class)
+    ->group(function() {
+      Route::get('/',  'index')->name('index');
+      Route::get('create', 'create');
+      Route::post('store', 'store')->name('codes.store');
+      Route::get('delete', 'delete')->name('codes.delete');
+      Route::post('destroy', 'destroy')->name('codes.destroy');
 });
-
